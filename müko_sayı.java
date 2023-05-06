3749006934bada8875e86a1611cc21ca43143a36
TUGAY <108936202+tugaycay@users.noreply.github.com>
2023-05-06 16:45:20 +0300

    Add files via upload

---
+++ müko_sayı.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class müko_sayı {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int sayi,toplam=0;
+        System.out.println("Sayı giriniz:");
+        sayi=input.nextInt();
+        for(int i=1;i<sayi;i++){
+            if(sayi%i==0){
+                toplam=toplam+i;
+            }
+        }if(toplam==sayi){
+            System.out.println(sayi+" Mükemmel sayıdır.");
+        }else {
+            System.out.println(sayi+" Mükemmel sayı değildir.");
+        }
+    }
+}
