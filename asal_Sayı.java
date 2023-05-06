83d69bc64ab221b061a3f40dd6c4adba29d7e0bb
TUGAY <108936202+tugaycay@users.noreply.github.com>
2023-05-06 22:31:56 +0300

    Add files via upload

---
+++ asal_Sayı.java
@@ -0,0 +1,21 @@
+public class asal_Sayı {
+    public static void main(String[]args){
+        int a=100;
+        int sayı=1;
+        for(int k=1;k<=100;k++){
+            if(k%2==1 || k==2){
+                System.out.println(k+" ASAL SAYIDIR.");
+            }
+        }
+
+                /* DO WHİLE DÖNGÜSÜYÜLE
+        do{
+           k++;
+           if(k%2==1 || k==2){
+               System.out.println(k+" ASAL SAYIDIR.");
+           }
+
+        }while(k<a);
+            */
+    }
+}
