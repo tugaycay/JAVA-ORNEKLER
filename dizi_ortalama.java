0618b9a4f12422ec6c9aaf24ed4c91879553c995
TUGAY <108936202+tugaycay@users.noreply.github.com>
2023-05-16 13:52:34 +0300

    Add files via upload

---
+++ dizi_ortalama.java
@@ -0,0 +1,21 @@
+package Diziler;
+public class dizi_ortalama {
+    public static void main(String[]args){
+ int dizi []= {1,2,3,4,5};
+ int sum =0;
+ for (int i =0;i<dizi.length;i++){
+     sum+=dizi[i];
+ }
+ System.out.println("DİZİNİN TOPLAMI: "+ sum );
+        System.out.println("DİZİNİN ORTALAMASI: "+ sum/dizi.length );
+
+
+
+
+
+
+
+    }
+
+
+}
